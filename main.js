e8592fd42dcae7b35c9b2c309044e256b475ca39
sanaz <sanazmoeini2019@gmail.com>
2023-09-13 16:43:49 +0330

    final commit?

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import './style.css'
-import 'bootstrap'
-/* import 'bootstrap/dist/js/bootstrap.bundle.min' */
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min'
 
 import fetchNav from './public/js/module/Header/fetchNav'
 
