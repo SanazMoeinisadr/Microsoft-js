81a1b379ccdb4a207b362cbce85226f201bf790d
sanaz <sanazmoeini2019@gmail.com>
2023-09-13 16:39:53 +0330

    final commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig ({
-       base: '/microsoft-js/',
+       base: '/Microsoft-js/',
 
        build:{
               target : 'esnext'
